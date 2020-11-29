711ba6331d373fc74a137778d7569f9df9ebc9da
Cuong <cuongle@tamdongtam.vn>
2020-11-29 16:04:54 +0700

    Add new time sheet script

--- timesheet.py
+++ timesheet.py
@@ -1,12 +1,18 @@
+import calendar
+import csv
+
 from openpyxl import load_workbook
 from openpyxl.comments import Comment
-import csv
 from datetime import time, date, datetime, timedelta
+import datetime as dt
+
+month = datetime.now().month
 
 
+IN_FILE = f'2020-{month}-21.csv'
+export_day = IN_FILE[0:10]
 SAMPLE_FILE = 'WorkingdayTemplate.xlsx'
 OUT_FILE = 'Workingday.xlsx'
-IN_FILE = 'BangChamCong.csv'
 
 STATUS_OK = ''
 STATUS_DAY_OFF = 1
@@ -32,22 +38,37 @@ def time2str(value):
 def str2date(value):
     if isinstance(value, (date, datetime)):
         return value
-    return datetime.strptime(value, '%Y/%m/%d')
+    return datetime.strptime(value, '%Y-%m-%d')
 
 
 def str2datetime(value):
     if isinstance(value, (date, datetime)):
         return value
-    return datetime.strptime(value, '%Y/%m/%d %H:%M:%S')
+    return datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
 
 
-print(f'Start loading check time from {IN_FILE}')
+def get_late_time(time: float) -> float:
+    late_time: float
+    if time >= 8:
+        return 8
+    if time <= 0.25:
+        late_time = 0.25
+    elif 0.25 < time <= 0.5:
+        late_time = 0.5
+    elif 0.5 < time <= 0.75:
+        late_time = 0.75
+    else:
+        late_time =time
+    return late_time
 
-first_day = time2str('2020-06-21')
-last_day = time2str('2020-07-21')
 
+datetimeFormat = '%Y-%m-%d %H:%M:%S'
+first_day = time2str(f'{export_day}')
 start = datetime.strptime(first_day, '%Y-%m-%d')
-end = datetime.strptime(last_day, '%Y-%m-%d')
+
+days_in_month = calendar.monthrange(start.year, start.month)[1]
+plus_day = timedelta(days=days_in_month - 1)
+end = start + plus_day
 step = timedelta(days=1)
 
 data = {}
@@ -71,89 +92,71 @@ print(working_days)
 
 with open(IN_FILE, mode='r', encoding='utf-8', ) as file:
     reader = csv.reader(file)
-    for emp in reader:
-        emp_id = emp[0]
-        emp_name = emp[1]
-        check_in = emp[2]
-        check_out = emp[3]
-        late_time = emp[4].replace(',', '.')
-
+    for row in reader:
+        emp_id = row[0].upper()
+        emp_name = row[1].upper()
         key = (emp_id, emp_name)
         if key not in data:
             data[key] = []
-        day = str2date(check_in[0:10])
-        check_in_time = str2datetime(check_in)
+        day = str2datetime(row[2])
+        right_time_str = f'{str(row[2])[0:10]} 08:30:00'
+        noon_time_str = f'{str(row[2])[0:10]} 13:00:00'
+        late_time_raw = dt.datetime.strptime(str(row[2])[0:19], datetimeFormat) \
+                    - dt.datetime.strptime(right_time_str, datetimeFormat)
+        if late_time_raw.seconds > 16200:
+            late_time_raw = dt.datetime.strptime(str(row[2])[0:19], datetimeFormat) \
+                    - dt.datetime.strptime(noon_time_str, datetimeFormat) \
+                    + timedelta(hours=4)
+        late_time_real = (late_time_raw.seconds / 3600) + late_time_raw.days * 3600
+        late_time = get_late_time(late_time_real)
+        data[key].append((day, late_time, late_time_real))
+        print(data[key])
 
-        data[key].append((day, late_time, check_in_time))
 
+print(f'Start loading template from {SAMPLE_FILE}')
+out_wb = load_workbook(SAMPLE_FILE)
+out_ws = out_wb.active
+k = 0  # column
+start_day = datetime.strptime(first_day, '%Y-%m-%d')
+while start_day <= end:
+    column = 6 + k
+    out_ws.cell(row=6, column=column, value=start_day.strftime("%a"))
+    out_ws.cell(row=7, column=column, value=start_day.day)
 
-i = 0  # row
-status = ''
-for emp_id, emp_name in sorted(data):
-    row = 8 + i
-    out_ws.cell(row=row, column=1).value = i + 1
-    out_ws.cell(row=row, column=2).value = emp_id
-    out_ws.cell(row=row, column=3).value = emp_name
+    i = 0  # row
+    for emp_id, emp_name in sorted(data):
+        row = 8 + i
+        out_ws.cell(row=row, column=1).value = i + 1
+        out_ws.cell(row=row, column=2).value = emp_id
+        out_ws.cell(row=row, column=3).value = emp_name
 
-    check_time = sorted(data[(emp_id, emp_name)])
-    wk = 0
-    for day, late_time, check_in_time in check_time:
-        column = 6 + k
-        cell = out_ws.cell(row=row, column=column)
-        time = check_in_time.strftime('%H:%M:%S')
-        if emp_id in IGNORE_EMP_ID:
-            status = STATUS_OK
-            continue
-        elif day.weekday() > 4:
-            status = STATUS_OK
-        elif day.strftime('%d') != start.strftime('%d'):
-            continue
-        elif float(late_time) > 0:
-            status = float(late_time) / 8
-            cell.comment = Comment(f'TDT STAFF:\n đi muộn lúc: {time}', 'Tool')
-        else:
-            status = 0
-        cell.value = status
-        wk += 1
-    sum_cell = out_ws.cell(row=row, column=37)
-    sum_cell.value = f'=SUM(F{row}:AJ{row})'
+        check_time = data[(emp_id, emp_name)]
+        for day, late_time, late_time_real in check_time:
+            # Column start from 6 because there 2 hidden columns
+            cell = out_ws.cell(row=row, column=column)
+            time = day.strftime('%H:%M:%S')
+            if emp_id in IGNORE_EMP_ID:
+                status = STATUS_OK
+            elif day.weekday() > 5:
+                status = STATUS_OK
+            elif day.strftime('%d') != start_day.strftime('%d'):
+                continue
+            elif late_time_real > 0 and late_time > 0:
+                status = late_time / 8
+                cell.comment = Comment(f'TDT STAFF:\n đi muộn lúc: {time}', 'Tool')
+            else:
+                status = 0
+            if status != '':
+                cell.value = status
+                print(f"{cell} --- {status}")
+        sum_cell = out_ws.cell(row=row, column=38)
+        sum_cell.value = f'=SUM(F{row}:AJ{row})'
 
-    i += 1
+        i += 1
+
+    k += 1
+    start_day += step
 
 print(f'Saving working day to {OUT_FILE}')
 out_wb.save(OUT_FILE)
 print('DONE.')
-#     i = 0  # row
-#     for emp_id, emp_name in sorted(data):
-#         row = 8 + i
-#         out_ws.cell(row=row, column=1).value = i + 1
-#         out_ws.cell(row=row, column=2).value = emp_id
-#         out_ws.cell(row=row, column=3).value = emp_name
-#
-#         check_time = data[(emp_id, emp_name)]
-#         for day, late_time, check_in_time in sorted(check_time):
-#             # Column start from 6 because there 2 hidden columns
-#             cell = out_ws.cell(row=row, column=column)
-#             time = check_in_time.strftime('%H:%M:%S')
-#             if emp_id in IGNORE_EMP_ID:
-#                 status = STATUS_OK
-#                 continue
-#             elif day.weekday() > 4:
-#                 status = STATUS_OK
-#             elif day.strftime('%d') != start.strftime('%d'):
-#                 continue
-#             elif float(late_time) > 0:
-#                 status = float(late_time) / 8
-#                 cell.comment = Comment(f'TDT STAFF:\n đi muộn lúc: {time}', 'Tool')
-#             else:
-#                 status = 0
-#             cell.value = status
-#         sum_cell = out_ws.cell(row=row, column=37)
-#         sum_cell.value = f'=SUM(F{row}:AJ{row})'
-#
-#         i += 1
-#
-#
-
-
-
---
+++ timesheet_bak.py
@@ -0,0 +1,164 @@
+from openpyxl import load_workbook
+from openpyxl.comments import Comment
+import csv
+import pandas
+from datetime import time, date, datetime, timedelta
+
+
+SAMPLE_FILE = 'WorkingdayTemplate.xlsx'
+OUT_FILE = 'Workingday.xlsx'
+IN_FILE = 'BangChamCong.csv'
+
+STATUS_OK = ''
+STATUS_DAY_OFF = 1
+
+IGNORE_EMP_ID = [
+    'ECO0001',  # CEO
+    'ECO0003',  # A. Trung Bom
+    'ECO0012',  # Ma Nhu
+    'ECO0038',  # Chi Chung
+    'ECO0089',  # Vuong
+    'ECO0345',  # Co Sam
+]
+
+
+def time2str(value):
+    if isinstance(value, str):
+        return value
+    if isinstance(value, time):
+        return value.strftime('%d')
+    return ''
+
+
+def str2date(value):
+    if isinstance(value, (date, datetime)):
+        return value
+    return datetime.strptime(value, '%Y/%m/%d')
+
+
+def str2datetime(value):
+    if isinstance(value, (date, datetime)):
+        return value
+    return datetime.strptime(value, '%Y/%m/%d %H:%M:%S')
+
+
+print(f'Start loading check time from {IN_FILE}')
+
+first_day = time2str('2020-06-21')
+last_day = time2str('2020-07-21')
+
+start = datetime.strptime(first_day, '%Y-%m-%d')
+end = datetime.strptime(last_day, '%Y-%m-%d')
+step = timedelta(days=1)
+
+data = {}
+
+working_days = []
+
+
+print(f'Start loading template from {SAMPLE_FILE}')
+out_wb = load_workbook(SAMPLE_FILE)
+out_ws = out_wb.active
+
+k = 0  # column
+while start <= end:
+    column = 6 + k
+    out_ws.cell(row=6, column=column, value=start.strftime("%a"))
+    out_ws.cell(row=7, column=column, value=start.day)
+    working_days.append(start)
+    k += 1
+    start += step
+print(working_days)
+
+reader = pandas.read_csv('BangChamCong.csv', index_col='Employee/Employee code', parse_dates=['Check In', 'Check Out'])
+for emp in reader:
+    emp_id = emp[0]
+    emp_name = emp[1]
+    check_in = emp[2]
+    check_out = emp[3]
+    late_time = emp[4].replace(',', '.')
+
+    key = (emp_id, emp_name)
+    if key not in data:
+        data[key] = []
+    day = check_in[0:10]
+    # check_in_time = str2datetime(check_in)
+    #
+    # data[key].append((day, late_time, check_in_time))
+    print(data[key])
+
+i = 0  # row
+status = ''
+for emp_id, emp_name in sorted(data):
+    row = 8 + i
+    out_ws.cell(row=row, column=1).value = i + 1
+    out_ws.cell(row=row, column=2).value = emp_id
+    out_ws.cell(row=row, column=3).value = emp_name
+
+    check_time = sorted(data[(emp_id, emp_name)])
+    wk = 0
+    for day, late_time, check_in_time in check_time:
+        column = 6 + k
+        cell = out_ws.cell(row=row, column=column)
+        time = check_in_time.strftime('%H:%M:%S')
+        if emp_id in IGNORE_EMP_ID:
+            status = STATUS_OK
+            continue
+        elif day.weekday() > 4:
+            status = STATUS_OK
+        elif day.strftime('%d') != start.strftime('%d'):
+            continue
+        elif float(late_time) > 0:
+            status = float(late_time) / 8
+            cell.comment = Comment(f'TDT STAFF:\n đi muộn lúc: {time}', 'Tool')
+        else:
+            status = 0
+        cell.value = status
+        wk += 1
+    sum_cell = out_ws.cell(row=row, column=37)
+    sum_cell.value = f'=SUM(F{row}:AJ{row})'
+
+    i += 1
+
+print(f'Saving working day to {OUT_FILE}')
+out_wb.save(OUT_FILE)
+print('DONE.')
+
+
+
+
+
+#     i = 0  # row
+#     for emp_id, emp_name in sorted(data):
+#         row = 8 + i
+#         out_ws.cell(row=row, column=1).value = i + 1
+#         out_ws.cell(row=row, column=2).value = emp_id
+#         out_ws.cell(row=row, column=3).value = emp_name
+#
+#         check_time = data[(emp_id, emp_name)]
+#         for day, late_time, check_in_time in sorted(check_time):
+#             # Column start from 6 because there 2 hidden columns
+#             cell = out_ws.cell(row=row, column=column)
+#             time = check_in_time.strftime('%H:%M:%S')
+#             if emp_id in IGNORE_EMP_ID:
+#                 status = STATUS_OK
+#                 continue
+#             elif day.weekday() > 4:
+#                 status = STATUS_OK
+#             elif day.strftime('%d') != start.strftime('%d'):
+#                 continue
+#             elif float(late_time) > 0:
+#                 status = float(late_time) / 8
+#                 cell.comment = Comment(f'TDT STAFF:\n đi muộn lúc: {time}', 'Tool')
+#             else:
+#                 status = 0
+#             cell.value = status
+#         sum_cell = out_ws.cell(row=row, column=37)
+#         sum_cell.value = f'=SUM(F{row}:AJ{row})'
+#
+#         i += 1
+#
+#
+
+
+
